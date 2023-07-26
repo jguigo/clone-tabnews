a214b7ee4b0ceeed887e4421458af6ab0d980ee1
Guilherme Melo <83793609+jguigo@users.noreply.github.com>
2023-07-26 22:58:28 +0000

    adiciona pagina inicial

---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function Home() {
+    return <h1>Com todo o amor, apoio e orientação que vocês me deram ao longo da vida, me tornei alguém forte o suficiente para enfrentar qualquer desafio e buscar os meus sonhos. Sou eternamente grato por serem os melhores pais do mundo!</h1>
+}
+
+export default Home;
